3ff0eb45a53e06bc8fdeacefd8c559d3f0e902cd
Rahul Singh Rawat <11Rahulsinghrawat@gmail.com>
2025-08-25 09:55:37 +0530

    added Blog page and List of question answer

---
+++ src/app/blog/page.jsx
@@ -0,0 +1,17 @@
+
+const data = [
+  {
+    question:"",
+    answer:""
+  }
+]
+
+const Assignment = () => {
+  return (
+    <div>
+      lk;shdjk
+    </div>
+  )
+}
+
+export default Assignment;
---
+++ src/lib/Blogs.ts
@@ -0,0 +1,19 @@
+interface IBlogs{
+    question:string,
+    answer:string,
+}
+
+const BlogsList:IBlogs[]=[
+    {
+        question:'Explain Chat GPT to five year old?',
+        answer:"Chat GPT is a gini which give solution to the problems.It will always give solution which according to him is accurate, so don't blindliy trust the solution provided by it you may consider its suggestion while making any decision. The GPT doesn'yt have any emotions or feeling so it doesn't consider if you feel bad or good about the answers it gives."
+    },
+    {
+        question:'Explain vector embedding to your mom?',
+        answer:"Vector embedding is a method to place something in a ceratin position such that the things around it are related to it most in order of the distance from that certain thing. For example: Like we organize the kitchen our kitchen in such a way that if we want to look a ceratin thing we will know around which area it will be , we might not know the exact postion but we will know where to search for first instead of whole kitchen one by one"
+    },
+    {
+        question:"Explain tokenization to a fresher?",
+        answer:"",
+    }
+]
