312a5f3bd85ed2fcc9508ebe6061fd3c26e20e87
Rahul Singh Rawat <11Rahulsinghrawat@gmail.com>
2025-08-27 12:55:41 +0530

    build fixingg

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    resolveExtensions: ['.tsx', '.ts', '.jsx', '.js', '.json'],
+    resolveAlias: {
+      '@': './src',
+    },
+  },
 };
 
 export default nextConfig;
